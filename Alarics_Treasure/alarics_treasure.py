9a752c65b82cd9170d6d852418eab51b2f36df06
moh-patel <54450804+moh-patel@users.noreply.github.com>
2021-04-24 13:12:38 +0100

    Update alarics_treasure.py

--- Alarics_Treasure/alarics_treasure.py
+++ Alarics_Treasure/alarics_treasure.py
@@ -24,38 +24,3 @@ class Solution:
 
         return(sum)
 
-
-# parse_roman_numerals("MCMLXI")
-
-
-# def test1(self):
-#         solution = Solution()
-#         self.assertEqual(solution.parse_roman_numerals("MCMLXI"), 1961)
-
-#     def test2(self):
-#         solution = Solution()
-#         self.assertEqual(solution.parse_roman_numerals("III"), 3)
-
-#     def test3(self):
-#         solution = Solution()
-#         self.assertEqual(solution.parse_roman_numerals("IV"), 4)
-
-#     def test4(self):
-#         solution = Solution()
-#         self.assertEqual(solution.parse_roman_numerals("IX"), 9)
-
-#     def test5(self):
-#         solution = Solution()
-#         self.assertEqual(solution.parse_roman_numerals("LVIII"), 58)
-
-#     def test6(self):
-#         solution = Solution()
-#         self.assertEqual(solution.parse_roman_numerals("MCMXCIV"), 1994)
-
-#     def test7(self):
-#         solution = Solution()
-#         self.assertEqual(solution.parse_roman_numerals("LXXIII"), 73)
-
-#     def test8(self):
-#         solution = Solution()
-#         self.assertEqual(solution.parse_roman_numerals("MMXV"), 2015)
