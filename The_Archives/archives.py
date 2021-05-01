f3dbf9eaa25361e6284f3e31032452cc415ce3e6
moh-patel <54450804+moh-patel@users.noreply.github.com>
2021-05-01 19:35:31 +0100

    Update archives.py

--- The_Archives/archives.py
+++ The_Archives/archives.py
@@ -1,5 +1,17 @@
+import re
 class Solution:
 
     def get_check_digit(self, input):
-        # Your code goes here
-        pass
+        check = re.match("[0-9]-[0-9]{2}-[0-9]{6}-[a-z]$", input)
+        if check:
+            i = 0
+            # while(i<9):
+
+            total = 0
+            i = 1
+            for pos in input:
+                if pos.isdecimal():
+                    total += i*int(pos)
+                    i += 1
+            return total % 11
+        return False
